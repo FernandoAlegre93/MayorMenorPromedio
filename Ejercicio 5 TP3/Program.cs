b541c3336f9b449d96281c7a4a4efdd63188be3a
Fernando <feralegre777@hotmail.com>
2021-06-12 18:57:27 -0300

    Subiendo

---
+++ Ejercicio 5 TP3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_5.TP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int acum=0, i, prom;
+            int [] num = new int[15];
+            for (i = 0; i < 15; i++)
+            {
+                Console.WriteLine("Ingrese un numero");
+                num[i] = Int32.Parse(Console.ReadLine());
+
+                acum = acum + num[i];
+
+            }
+
+            Array.Sort(num);
+            prom= (acum/ 15);
+            Console.WriteLine("El numero mayor es : {0}", num[14]);
+            Console.WriteLine("El numero menor es : {0}", num[0]);
+            Console.WriteLine("El promedio es: {0}", prom );
+
+            Console.ReadKey();
+        }
+    }
+}
